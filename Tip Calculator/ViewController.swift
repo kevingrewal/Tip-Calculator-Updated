633f63ec463443682c6dd48aa8adfc7ef5c682b2
Kevin Grewal <kevin.grewal@sjsu.edu>
2019-12-20 00:01:07 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Kevin Grewal on 12/20/19.
+//  Copyright © 2019 Kevin Grewal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
